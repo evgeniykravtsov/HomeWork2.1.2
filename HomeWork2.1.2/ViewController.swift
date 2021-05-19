7c6f1a918198caafea433ce0b5e5513660c86509
Kravtsov Evgeniy <ekravtsov@ozon.travel>
2021-05-19 19:38:50 +0300

    Initial Commit

---
+++ HomeWork2.1.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork2.1.2
+//
+//  Created by Kravtsov Evgeniy on 19.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
