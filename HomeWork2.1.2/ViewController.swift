6585278aee95f6838a7abeff2446eccecd7a7f3b
Kravtsov Evgeniy <ekravtsov@ozon.travel>
2021-05-19 23:09:59 +0300

    Add button handler

--- HomeWork2.1.2/ViewController.swift
+++ HomeWork2.1.2/ViewController.swift
@@ -8,12 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var Red: UIView!
+    @IBOutlet weak var Yellow: UIView!
+    @IBOutlet weak var Green: UIView!
+    @IBOutlet weak var buttonStart: UIButton!
+    let radius:CGFloat = 50
+    let fadedLight:CGFloat = 0.3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        Red.layer.cornerRadius = radius
+        Red.alpha = fadedLight
+        Yellow.layer.cornerRadius = radius
+        Yellow.alpha = fadedLight
+        Green.layer.cornerRadius = radius
+        Green.alpha = fadedLight
+
         // Do any additional setup after loading the view.
     }
 
 
+    
+    @IBAction func handleButtonClick() {
+        
+        let light:CGFloat = 1.0
+        switch light {
+        case Red.alpha:
+            Yellow.alpha = light
+            Red.alpha = fadedLight
+        case Yellow.alpha :
+            Green.alpha = light
+            Yellow.alpha = fadedLight
+        case Green.alpha:
+            Red.alpha = light
+            Green.alpha = fadedLight
+        default:
+            buttonStart.setTitle("Next", for: .normal)
+            Red.alpha = light
+        }
+    }
+    
 }
 
